ee739a9e99f070e7edcb10d3451e48622ef6949e
neyluu <75064225+neyluu@users.noreply.github.com>
2026-04-01 18:22:34 +0200

    Removed conflicting code

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,20 +3,6 @@
     import favicon from '$lib/assets/favicon.svg';
 
     let {children} = $props();
-
-    // TODO
-    // Test endpoint call, should be removed later
-    import {onMount} from "svelte";
-
-    let data;
-
-    onMount(async () => {
-        const res = await fetch("/api/test");
-        data = await res.json();
-        console.log(data)
-    });
-    //=================================
-
 </script>
 
 <svelte:head>
