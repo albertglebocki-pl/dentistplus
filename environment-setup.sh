96cfff3d9ccffee4c38c61fc79b1887856ed6d52
Albert G. <252793@edu.p.lodz.pl>
2026-04-20 11:12:34 +0200

    Added the required environmental variable for storage to create the bucket properly

--- environment-setup.sh
+++ environment-setup.sh
@@ -5,6 +5,7 @@ STORAGE_RPC_SECRET=$(openssl rand -hex 32)
 STORAGE_ADMIN_TOKEN=$(openssl rand -hex 32)
 STORAGE_KEY_ID=$(openssl rand -hex 10)
 STORAGE_SECRET_KEY=$(openssl rand -hex 20)
+STORAGE_BUCKET=patient-images
 POSTGRES_USER=admin
 POSTGRES_PASSWORD=$(openssl rand -hex 32)
 POSTGRES_DB=dentistplus
