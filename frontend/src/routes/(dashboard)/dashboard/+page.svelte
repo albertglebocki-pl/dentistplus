667c6e5bbba36f7400ce383d16bc2730c30f8f1e
neyluu <75064225+neyluu@users.noreply.github.com>
2026-04-21 09:46:35 +0200

    Added basic dashboard component structure

--- frontend/src/routes/(dashboard)/dashboard/+page.svelte
+++ frontend/src/routes/(dashboard)/dashboard/+page.svelte
@@ -2,7 +2,17 @@
     import type { PageData } from "./$types";
 
     const { data }: { data: PageData } = $props();
+    const role = data.user.role;
+
+    import AdminDashboard from "$lib/components/dashboard/admin/AdminDashboard.svelte";
+    import DoctorDashboard from "$lib/components/dashboard/doctor/DoctorDashboard.svelte";
+    import UserDashboard from "$lib/components/dashboard/user/UserDashboard.svelte";
 </script>
 
-<main>DASHBOARD</main>
-<p>{data.user.role}</p>
+{#if role === "ADMIN"}
+    <AdminDashboard user={data.user}/>
+{:else if role === "USER"}
+    <UserDashboard user={data.user}/>
+{:else}
+    <DoctorDashboard user={data.user}/>
+{/if}
