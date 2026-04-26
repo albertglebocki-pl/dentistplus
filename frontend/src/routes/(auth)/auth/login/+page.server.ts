1ac32dc32198c08eaa053668237e706dea742244
Albert G. <252793@edu.p.lodz.pl>
2026-04-26 14:27:51 +0200

    Added the remember me funcionality

--- frontend/src/routes/(auth)/auth/login/+page.server.ts
+++ frontend/src/routes/(auth)/auth/login/+page.server.ts
@@ -1,31 +1,32 @@
-import {fail, redirect} from '@sveltejs/kit';
-import type {Actions} from './$types';
+import { fail, redirect } from "@sveltejs/kit";
+import type { Actions } from "./$types";
 import api from "$lib/server/utils/api";
 
 export const actions: Actions = {
-    default: async ({request, cookies}) => {
-        const data = await request.formData();
-        const email = data.get('email');
-        const password = data.get('password');
+  default: async ({ request, cookies }) => {
+    const data = await request.formData();
+    const email = data.get("email");
+    const password = data.get("password");
+    const rememberMe = data.get("remember");
 
-        const res = await fetch(api("/auth/login"), {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({email, password}),
-        });
+    const res = await fetch(api("/auth/login"), {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password, rememberMe }),
+    });
 
-        const result = await res.json();
+    const result = await res.json();
 
-        if (!res.ok) {
-            return fail(401, {error: result.error || "Invalid credentials", email});
-        }
-
-        cookies.set("token", result.token, {
-            path: '/',
-            httpOnly: true,
-            sameSite: 'lax',
-        });
-
-        throw redirect(303, "/dashboard");
+    if (!res.ok) {
+      return fail(401, { error: result.error || "Invalid credentials", email });
     }
+
+    cookies.set("token", result.token, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+    });
+
+    throw redirect(303, "/dashboard");
+  },
 };
