24667bf04035e79a004c91caa148c6d296a15232
neyluu <75064225+neyluu@users.noreply.github.com>
2026-04-29 23:17:22 +0200

    Added styles indexes and style for selecting day + refactor

--- frontend/src/lib/components/dashboard/utils/Calendar.svelte
+++ frontend/src/lib/components/dashboard/utils/Calendar.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-    let {visits = [], fullSlots = []} = $props();
+    let {
+        visits = [],
+        fullSlots = []
+    } = $props();
 
     const days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
     const months = [
@@ -17,7 +20,8 @@
         "December",
     ];
 
-    function getMonday(date: Date) {
+
+    const getMonday = (date: Date) => {
         const d = new Date(date);
         const day = (d.getDay() + 6) % 7; // Mon = 0
         d.setDate(d.getDate() - day);
@@ -25,7 +29,7 @@
         return d;
     }
 
-    function getWeekDays(start: Date) {
+    const getWeekDays = (start: Date) => {
         return Array.from({length: 5}, (_, i) => {
             const d = new Date(start);
             d.setDate(start.getDate() + i);
@@ -39,15 +43,18 @@
     const weekDays = $derived(getWeekDays(currentWeekStart));
     const currentMonthLabel = $derived(months[weekDays[0].getMonth()]);
 
+    let selectedDate: Date | null = $state(null);
+
     const calendarData = $derived.by(() => {
         const grid = Array.from({length: 5}, () => ({
-            taken: [],
-            mine: [],
+            taken: [] as number[],
+            mine: [] as number [],
         }));
 
         const start = new Date(currentWeekStart);
         const end = new Date(start);
         end.setDate(start.getDate() + 5);
+
         fullSlots.forEach((slot: any) => {
             const d = new Date(slot.dateTime);
             if (d >= start && d < end) {
@@ -71,16 +78,26 @@
         return grid;
     });
 
-    function changeWeek(offset: number) {
+    const changeWeek = (offset: number) => {
         const nextDate = new Date(currentDate);
         nextDate.setDate(nextDate.getDate() + offset);
         currentDate = nextDate;
+
+        selectedDate = null;
+    }
+
+    const handleSelect = (datetime: Date) => {
+        if (selectedDate === datetime) {
+            selectedDate = null;
+            return;
+        }
+        selectedDate = datetime
     }
 </script>
 
 <div class="flex gap-2">
     <div
-            class="week-switch flex flex-col items-center justify-between p-2 font-bold"
+        class="week-switch flex flex-col items-center justify-between p-2 font-bold"
     >
         <button onclick={() => changeWeek(-7)}>↑</button>
 
@@ -93,7 +110,7 @@
     </div>
 
     <div
-            class="current-week grid grid-rows-5 gap-2 w-18 shrink-0 bg-primary text-secondary rounded p-2"
+        class="current-week grid grid-rows-5 gap-2 w-18 shrink-0 bg-primary text-secondary rounded p-2"
     >
         {#each weekDays as dayDate, i}
             <div class="flex flex-col justify-center items-center">
@@ -108,24 +125,33 @@
 
     <div class="calendar flex-1 grid grid-cols-11 grid-rows-5 gap-2">
         {#each weekDays as dayDate, dayIndex}
-            {#each Array.from({length: 11}, (_, h) => 8 + h) as hour}
-                {@const isMine = (calendarData[dayIndex]?.mine ?? []).includes(
-                    hour,
-                )}
-                {@const isTaken = (
-                    calendarData[dayIndex]?.taken ?? []
-                ).includes(hour)}
+            {#each Array.from({length: 11}, (_, h) => {
+                const hour = 8 + h;
 
-                <div
-                        class="rounded text-center flex flex-col justify-between border p-0.5
-                    {isMine
-                        ? 'bg-yellow-100 border-yellow-400'
-                        : isTaken
-                          ? 'bg-red-50 border-red-200 opacity-80'
-                          : 'bg-white border-gray-300'}"
+                const dateTime = new Date(dayDate);
+                dateTime.setHours(hour, 0, 0, 0);
+
+                return {
+                    hour,
+                    dateTime
+                };
+            }) as slot}
+                {@const isMine = (calendarData[dayIndex]?.mine ?? []).includes(slot.hour)}
+                {@const isTaken = (calendarData[dayIndex]?.taken ?? []).includes(slot.hour)}
+
+                <button
+                    type="button"
+                    class="rounded text-center flex flex-col justify-between border p-0.5 hover:bg-gray-200
+                        {isMine
+                            ? 'bg-yellow-100 border-yellow-400'
+                            : isTaken
+                                ? 'bg-red-50 border-red-200 opacity-80'
+                                : 'bg-white border-gray-300'}
+                        {selectedDate === slot.dateTime ? 'border-primary border-2' : ''}"
+                    onclick={() => handleSelect(slot.dateTime)}
                 >
                     <div
-                            class="h-1.5 w-full rounded-full
+                        class="h-1.5 w-full rounded-full
                         {isMine
                             ? 'bg-yellow-500'
                             : isTaken
@@ -133,9 +159,9 @@
                               : 'bg-green-400'}"
                     ></div>
 
-                    <p>{hour}</p>
+                    <p>{slot.hour}</p>
                     <div class="h-2 w-full"></div>
-                </div>
+                </button>
             {/each}
         {/each}
     </div>
